1d500079fbe6d89b77e2f1c4e458f2cd2c36c93d
Greg Bystroff <39066965+GregBystroff@users.noreply.github.com>
2020-11-23 01:58:09 -0700

    latest
    partial just for reference

---
+++ BarracksInventory/BarracksInventory/Models/EfUnitRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarracksInventory.Models
+{
+    public class EfUnitRepository
+        : IUnitRepository
+    {
+        //   F i e l d s   &   P r o p e r t i e s
+
+        private BarracksInventoryDbContext context;
+        //   C o n s t r u c t o r s
+
+        public EfUnitRepository(BarracksInventoryDbContext ctex)
+        {
+            context = ctex;
+        }
+
+        //   M e t h o d s
+        public IQueryable<Unit> Units => throw new NotImplementedException();
+
+        public IQueryable<Unit> GetAllUnits()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BarracksInventory/BarracksInventory/Models/IUnitRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace BarracksInventory.Models
+{
+    interface IUnitRepository
+    {
+        //   F i e l d s   and   P r o p e r t i e s
+        IQueryable<Unit> Units { get; }
+
+        // Constructors (not in an interface)
+        // Methods
+
+        public IQueryable<Unit> GetAllUnits();
+    }
+}
